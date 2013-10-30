483ba6c014c1f0c515e05ce4b8bf6b00d5b65ab0
Calvin Beck <hobbes@ualberta.ca>
2013-10-29 20:54:54 -0600

    Started the implementation of some of the Arduino functions. The pinMode function has been completed.

---
+++ arduino/Arduino.c
@@ -0,0 +1,42 @@
+b/* Copyright (C) 2013 Calvin Beck
+
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#include "Arduino.h"
+#include "commands.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  Implementation of the Arduino functions.
+ */
+
+void pinMode(uint8_t pin, uint8_t mode) {
+    /* Send PINMODE command identifier */
+    write(STDIO_FILENO, &PINMODE, 1);
+
+    /* Send the pin argument, and then the mode */
+    write(STDIO_FILENO, &pin, 1);
+    write(STDIO_FILENO, &mode, 1);
+}
