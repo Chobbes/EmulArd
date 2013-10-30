8f5cfa43b78c2cbda02ad4a8a493c13321a695ed
Calvin Beck <hobbes@ualberta.ca>
2013-10-29 21:58:43 -0600

    Implementation for random functions. Hurray modulo bias!

--- arduino/Arduino.c
+++ arduino/Arduino.c
@@ -87,3 +87,64 @@ void digitalWrite(uint8_t pin, int value) {
         total_sent++;
     }
 }
+
+
+int analogRead(uint8_t pin) {
+    /* Send ANALOG_READ command */
+    write(STDOUT_FILENO, &ANALOG_READ, sizeof(ANALOG_READ));
+
+    /* Send the pin number argument */
+    write(STDOUT_FILENO, &pin, sizeof(pin));
+
+    /* Receive the integer result */
+    int total_read = 0;
+    int our_int = 0;
+
+    while (total_read < sizeof(our_int)) {
+        char int_part;
+        ssize_t bytes_read = read(STDIN_FILENO, &int_part, sizeof(int_part));
+
+        if (1 == bytes_read) {
+            /* Stuff the byte we read into our integer */
+            our_int << sizeof(int_part);
+            our_int |= int_part;
+
+            total_read++;
+        }
+    }
+
+    return our_int;
+}
+
+
+void analogWrite(uint8_t pin, int value) {
+    /* Send DIGITAL_WRITE command */
+    write(STDOUT_FILENO, &DIGITAL_WRITE, sizeof(DIGITAL_WRITE));
+
+    /* Write our integer */
+    int shift_amount = (sizeof(value) - sizeof(int_part));
+
+    while (total_sent < sizeof(value)) {
+        char int_part = value >> shift_amount;
+
+        write(STDOUT_FILENO, &int_part, sizeof(int_part));
+
+        shift_amount -= sizeof(int_part);
+        total_sent++;
+    }
+}
+
+
+void randomSeed(unsigned int seed) {
+    srand(seed);
+}
+
+
+long random(long max) {
+    return rand() % max;
+}
+
+
+long random(long min, long max) {
+    return (rand() % (max - min)) + min;
+}
